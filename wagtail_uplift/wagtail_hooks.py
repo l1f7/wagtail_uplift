155004aa0d65b459496847e645cd8da6cfaa52e6
AdrianGyuricska <a.gyuricska@gmail.com>
2017-02-23 15:21:57 -0700

    Replace explore with pages

--- wagtail_uplift/wagtail_hooks.py
+++ wagtail_uplift/wagtail_hooks.py
@@ -7,3 +7,15 @@ from wagtail.wagtailcore import hooks
 def global_admin_css():
     html = '<link rel="stylesheet" href="{path}">'.format(path=static('css/wagtail_uplift.min.css'))
     return format_html(html)
+
+@hooks.register('register_admin_menu_item')
+def register_tile_menu_item():
+    return MenuItem(
+        'Pages',
+        '/cmsadmin/pages/',
+        classnames='icon icon-folder-open-1',
+        order=1)
+
+@hooks.register('construct_main_menu')
+def hide_explorer_menu_item(request, menu_items):
+    menu_items[:] = [item for item in menu_items if item.name != 'explorer']
